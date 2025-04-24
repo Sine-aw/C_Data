e1851d8118914d4f8d472e99be680e82a201da91
Sine-aw <siany9187@gmail.com>
2025-04-24 11:39:07 +0900

    주석 작성 추가

--- ex02/ex02.c
+++ ex02/ex02.c
@@ -3,24 +3,24 @@
 int fun1(); // 함수 원형(함수 선언)
 int fun2();
 
-int main(void) 
+int main(void) //void는 형 없음, 생략 가능
 {
 	printf("메인 시작\n"); //1
-	fun1(); //2  
+	fun1(); //2  함수 호출, 인수를 보내지 않음
 	
 	printf("메인 끝\n"); //10   
 	
-	return 0; //11
+	return 0; //11, 완전 종료
 }
 
-int fun1() 
+int fun1() //매개변수도 없음, return으로 반환할 값의 형을 기입한다 (return 0 -> 정수형 -> int)
 {
 	printf("fun1함수 시작\n"); //3
-	fun2(); //4
+	fun2(); //4, 함수 호출
 	
 	printf("fun1함수 끝\n");   //8
 	
-	return 0; //9
+	return 0; //9, fun1() 호출 다음 수행 줄로 이동
 }
 
 int fun2() 
@@ -28,5 +28,5 @@ int fun2()
 	printf("fun2함수 시작\n"); //5
 	printf("fun2함수 끝\n"); //6
 	
-	return 0; //7
+	return 0; //7, fun2() 호출 다음 수행 줄로 아동
 }
